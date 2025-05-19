ed9796c0aa0d71d75d19f6f250b7b3bb66225b99
BadlyacX <gary970509@gmail.com>
2025-05-19 18:41:32 +0800

    e

---
+++ src/main/java/com/badlyac/drawer/MapDrawerMain.java
@@ -0,0 +1,14 @@
+package com.badlyac.drawer;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(MapDrawerMain.MODID)
+public class MapDrawerMain {
+    public static final String MODID = "mapdrawer";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MapDrawerMain() {}
+
+}
